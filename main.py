527e01b4e7ce4a46edae14c84153693feededbd1
anderson <andinlucas8@gmail.com>
2023-09-30 14:24:45 -0300

    FEAT/ começando os trabalhos

---
+++ main.py
@@ -0,0 +1,16 @@
+## instalações
+# pip install SpeechRecognition PyAudio
+# pip install PyAudio
+# pip install spacy
+# python -m spacy download en_core_web_sm  # Modelo para inglês
+# pip install transformers
+# pip install pyttsx3
+
+## importações
+# import speech_recognition as sr
+# import pyaudio
+# import spacy
+# from transformers import pipeline
+# import pyttsx3
+
+##
