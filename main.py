5510912f4345177a9cfa86f10147c5b1372ffd27
anderson <andinlucas8@gmail.com>
2023-12-23 13:11:45 -0300

    feat/ main: Continuando código main inicial

--- main.py
+++ main.py
@@ -25,3 +25,65 @@ recognizer = sr.Recognizer()
 # Inicializar o mecanismo de síntese de fala
 engine = pyttsx3.init()
 nlp = spacy.load("en_core_web_sm")
+
+# Função para reconhecimento de voz
+def recognize_speech():
+    with sr.Microphone() as source:
+        print("Fale algo...")
+        audio = recognizer.listen(source)
+
+    try:
+        text = recognizer.recognize_google(audio)
+        print("Você disse:", text)
+        return text
+    except sr.UnknownValueError:
+        print("Não foi possível entender a fala")
+    except sr.RequestError as e:
+        print(f"Erro no reconhecimento de fala: {e}")
+
+def check_and_correct_code(code):
+    try:
+        # Tenta analisar o código
+        parsed_code = ast.parse(code)
+        return "O código está correto."
+    except SyntaxError as e:
+        return f"Erro de sintaxe: {e}"
+    except Exception as e:
+        return f"Erro: {e}"
+
+# Função para análise gramatical usando NLTK
+def analyze_text(text):
+    tokens = word_tokenize(text)
+    tagged_tokens = nltk.pos_tag(tokens)
+    return tagged_tokens
+
+# Função para síntese de fala
+def text_to_speech(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def create_and_improve_text(input_text):
+    # Implemente aqui as melhorias desejadas no texto
+    improved_text = input_text  # Neste exemplo, o texto não é alterado
+    return improved_text
+
+# Exemplo de uso
+while True:
+    command = input("Digite um comando: ")
+
+    doc = nlp(command)
+    if "code" in command:
+        code = input("Digite o código que deseja verificar e corrigir: ")
+        result = check_and_correct_code(code)
+        print(result)
+    elif "fale" in command:
+        recognized_text = recognize_speech()
+    elif "text" in command:
+        input_text = input("Digite o texto que deseja criar e melhorar: ")
+        improved_text = create_and_improve_text(input_text)
+        print("Texto melhorado:", improved_text)
+    elif "sintetize" in command:
+        text = input("Digite o texto que deseja sintetizar: ")
+        text_to_speech(text)
+    elif command == "sair":
+        break
