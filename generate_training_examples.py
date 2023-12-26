fb838527d65b6407f34669753842981fdbd2253e
anderson <andinlucas8@gmail.com>
2023-12-26 11:41:45 -0300

    feat/ generate_training: Gerando códigos

---
+++ generate_training_examples.py
@@ -0,0 +1,30 @@
+import random
+
+code_structures = [
+    "for i in range(10):",
+    "if x > 0:",
+    "def my_function():",
+    "while condition:",
+    "class MyClass:",
+]
+
+code_actions = [
+    "verifique o código para erros.",
+    "examine meu código em busca de problemas.",
+    "há algum erro no meu código?",
+]
+
+num_examples = 100
+
+generated_examples = []
+while len(generated_examples) < num_examples:
+    code_structure = random.choice(code_structures)
+    code_action = random.choice(code_actions)
+    generated_example = f"{code_structure}  # {code_action}"
+
+    if generated_example not in generated_examples:
+        generated_examples.append(generated_example)
+
+with open("nlu_training_data.md", "a") as file:
+    for example in generated_examples:
+        file.write(f"- {example}\n")
